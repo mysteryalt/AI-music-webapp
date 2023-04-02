97884d0038d2c31a7ce1176e1c0ba92b9a1e47a0
mysteryalt <86229694+mysteryalt@users.noreply.github.com>
2023-04-02 09:55:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+rightWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -12,9 +16,35 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses)
 }
 
 
 function draw() {
 image(video , 0 , 0 , 650 ,500);
 }
+
+
+function modelLoaded() {
+    console.log("posenet has loaded")
+    }
+    
+    function gotPoses(results) {
+    
+        if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X and Y= " + leftWristX + " " +  leftWristY );
+    
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right wrist X and Y = " + rightWristX + " " + rightWristY);    
+    
+    
+    }
+    
+       
+    }
